0f2d44338cfc647b8ed7eab362d501fbcb99d0bf
KarimAql <aqlkarim@gmail.com>
2022-12-03 20:51:01 +0200

    xml

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include "stdafx.h"
+//#using <mscorlib.dll>
+#include <tchar.h>
+#include "rapidxml.hpp"
+//#using <System.Xml.dll>
 
 using namespace std;
+//using namespace System::Xml;
+
 
 int main() {
+    
+
+
    
    return 0;
 }
