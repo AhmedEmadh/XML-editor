595d0fcc1ad9a98827237bc30a87f66dc073af1c
AhmedEmadh <104726251+AhmedEmadh@users.noreply.github.com>
2022-12-12 00:59:17 +0200

    uploaded XmlReadFile class and some includes

--- main.cpp
+++ main.cpp
@@ -9,9 +9,97 @@
 #include <string>
 #include <stack>
 
+//Ahmed Emad's includes
+#include <fstream>
+#include <sstream>
+#include <string>
 
 using namespace std;
 //using namespace System::Xml;
+
+/*
+XMLReadFile is written by Ahmed Emad
+please read carfully to understand the operation of the class:
+-the constructor takes relative path of the file
+-the content of the file is readed and encapsulated in the class
+-the method is_balanced returnes balance state (first stage to check correctness) and state takes values = {true, false}
+-the method is_correct returns the final state of the file (check that all stages returns true) and state takes values = {true, false }
+
+example of using the class:
+
+int main(){
+
+    XmlReadFile myfile("file.xml");
+    cout << myfile.is_correct() << endl;
+    return 0;
+}
+
+here:
+the program prints 1: if the file is balenced
+the program prints 0: if the file is not balenced
+*/
+
+class XmlReadFile{
+ private:
+    string content;
+    string readFileIntoString(const string& path) {
+    ifstream input_file(path);
+    if (!input_file.is_open()) {
+        cerr << "Could not open the file - '"
+             << path << "'" << endl;
+        exit(EXIT_FAILURE);
+    }
+    return string((std::istreambuf_iterator<char>(input_file)), std::istreambuf_iterator<char>());
+    }
+    string get_content(){
+        return this->content;
+    }
+    bool is_balenced(){
+        stack<char> stk;
+        for(int i=0;i<content.length();i++){
+            if(content[i] == '<'){
+                stk.push(content[i]);
+            } else if(content[i] == '>'){
+                if(!stk.empty()){
+                    stk.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        if(stk.empty()){
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+    bool is_valid(){
+        return true;
+    }
+    string to_json(){
+        return "not now";
+    }
+ public:
+    XmlReadFile (const string& path){
+        content = this->readFileIntoString(path);
+    }
+    
+    int sizeOfContent(){
+        return this->content.size();
+    }
+    bool is_correct(){
+        if((this->is_balenced()) && (this->is_valid())){
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
+
+
+
+
 bool balanced(const std::string& xml)
 {
     std::stack<std::string> tags;
